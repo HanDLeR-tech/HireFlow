13733ea31fcaa8e9c5ad7cfc73dcaaa33964e728
Aditya Sharma <04adityas@gmail.com>
2026-07-12 22:23:03 +0530

    fix:stream user

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,6 +1,6 @@
 import { StreamChat } from "stream-chat";
 import { ENV } from "./env.js";
-import { StreamClient} from "@stream-io/node-sdk";
+import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
@@ -9,13 +9,18 @@ if (!apiKey || !apiSecret) {
   throw new Error("STREAM_API_KEY and STREAM_API_SECRET must be set in the environment variables.");
 }
 
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret);//will be used for chat functionality
-export const streamClient = new StreamClient(apiKey, apiSecret);//will be used for video calling functionality
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); //will be used for chat functionality
+export const streamClient = new StreamClient(apiKey, apiSecret); //will be used for video calling functionality
 
 export const upsertStreamUser = async (userData) => {
   try {
     await chatClient.upsertUser(userData);
-    console.log(`Successfully upserted user ${userData} with ID: ${userData.id} to Stream.`);
+    console.log("Upserting user:", userData);
+
+    const response = await chatClient.upsertUser(userData);
+
+    console.log("Stream response:", response);
+    console.log("Successfully upserted user:", userData.id);
   } catch (error) {
     console.error("Error upserting user to Stream:", error);
   }
